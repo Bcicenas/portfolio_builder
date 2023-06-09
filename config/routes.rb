2249a02a0318cebd8ba629472fb1d06e97147a74
Benediktas <bcicenas@gmail.com>
2023-06-09 13:50:50 +0300

    mysql support and first controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "portfolios#index"
+  get '/portfolios', to: 'portfolios#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
