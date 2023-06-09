1e4db863f049a424854f63d4617653d94e79e5e1
Benediktas <bcicenas@gmail.com>
2023-06-09 15:36:34 +0300

    devise authentication fixes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,9 @@
 class PortfoliosController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+  end
+
+  def list
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
   root "portfolios#index"
   get '/portfolios', to: 'portfolios#index'
+  get '/portfolios/list', to: 'portfolios#list'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
