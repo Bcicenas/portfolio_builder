764b76f4128f8b1291b672e957ff5038425c1628
Benediktas <bcicenas@gmail.com>
2023-06-09 17:30:22 +0300

    basic functions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,9 +1,62 @@
 class PortfoliosController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
+  before_action :find_portfolio, only: [:edit, :update, :destroy]
+
   def index
   end
 
   def list
+    @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to @portfolio
+      flash[:notice] = 'Portfolio was successfully created'
+    else
+      flash[:alert] = 'Portfolio was not created'
+      render(:new) && (return false)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @portfolio.assign_attributes(portfolio_params)
+    if @portfolio.save
+      redirect_to @portfolio
+      flash[:notice] = 'Portfolio was successfully updated'
+    else
+      flash[:alert] = 'Portfolio was not updated'
+      render(:edit) && (return false) 
+    end   
+  end
+
+  def destroy
+    if @portfolio.destroy
+      redirect_to @portfolio
+      flash[:notice] = 'Portfolio was successfully deleted'
+    else
+      flash[:alert] = 'Portfolio was not deleted'
+      render(:edit) && (return false) 
+    end    
+  end
+
+  private
+
+  def find_portfolio
+    @portfolio = Portfolio.where(id: params[:id]).first
+    return if @portfolio
+  end
+
+  def portfolio_params
+    params.require(:portfolio).permit(:name)
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions' }, path: 'users', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'register' }
 
   root "portfolios#index"
   get '/portfolios', to: 'portfolios#index'
   get '/portfolios/list', to: 'portfolios#list'
+  resources :portfolios
+  # resolve('Portfolio') { [:portfolio] }
+  # get '/portfolios/new', to: 'portfolios#new'
+  # put '/portfolios/create', to: 'portfolios#create'
+  # get '/portfolios/edit', to: 'portfolios#edit'
+  # post '/portfolios/update', to: 'portfolios#update'
+  # delete '/portfolios/destroy', to: 'portfolios#destroy'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
